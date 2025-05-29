2066e54263660db0f15a4aca2348d70504d347d2
Makovey Vladislav <vladislav.makovey@raiffeisen.ru>
2025-05-29 17:50:45 +0300

    added base server listener

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	syslog "log"
+
+	"github.com/Makovey/booking_auth/internal/app"
+	"github.com/Makovey/booking_utils/pkg/config/env"
+	"github.com/Makovey/booking_utils/pkg/logger/slog"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fn := "main"
+
+	log := slog.NewLogger()
+	cfg, err := env.NewAuthConfig(log)
+	if err != nil {
+		syslog.Fatalf("[%s]: %w", fn, err)
+	}
+
+	appl := app.NewApp(log, cfg)
+	if err = appl.Run(); err != nil {
+		syslog.Fatalf("[%s]: %v", fn, err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Makovey/booking_auth
 
 go 1.23.4
+
+require github.com/Makovey/booking_utils v0.2.1
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
